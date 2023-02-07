bcb3f3076100e67fa69236069efd8a5a093c3bde
AlakaTahir <olawaletech2@gmail.com>
2023-02-07 20:21:57 +0100

    Unique Application

---
+++ Unique.Logic/MultiplicationLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unique.Model;
+
+namespace Unique.Logic
+{
+    public class MultiplicationLogic
+    {
+        public double Multiplication(MultiplicationModel model)
+        {
+            return model.NumA * model.NumB;
+        }
+    }
+}
